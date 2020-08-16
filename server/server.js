e24f197d13a242d65543794de83da357e58abc7c
HaraHiroshi <mrbob403@gmail.com>
2020-08-16 18:04:49 +0900

    save commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+/**
+ * Setup to use express.
+ */
+const express = require("express");
+const app = express();
+
+/**
+ * Resolve path.
+ */
+const path = require("path");
+const target = path.resolve("../dist/");
+
+/**
+ * Rooting for static files
+ */
+app.use(express.static(path.join(target)));
+
+/**
+ * Listen to port 3000.
+ */
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
+
+/**
+ * Resolve GET request.
+ */
+app.get("/", (req, res) => {
+  res.sendFile(target + "index.html");
+});
