6c056858e05a14d377532612da6d9e5d8c9ec280
HaraHiroshi <mrbob403@gmail.com>
2020-09-06 18:03:45 +0900

    save commit

--- server/server.js
+++ server/server.js
@@ -1,38 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-
-// Get connection of MongoDB(In case of use mongoose)
-const mongoose = require("mongoose");
-const url = "mongodb://127.0.0.1:27017/mybodywatch";
-mongoose.connect(url, { useNewUrlParser: true });
-const db = mongoose.connection;
-// define schema.
-const bodydataSchema = new mongoose.Schema(
-  {
-    userid: String,
-    weight: Number,
-    bmi: Number,
-    bfp: Number,
-    mm: Number,
-    kcal: Number,
-    date: String,
-  },
-  {
-    collection: "bodydata",
-  }
-);
-// compile to model from schema.
-const bodydata = mongoose.model("bodydata", bodydataSchema);
-
-db.once("open", () => {
-  console.log("Database connected:", url);
-  // find all document from db.
-});
-
-db.on("error", (err) => {
-  console.error("connection error:", err);
-});
+const model = require("./model");
+const bodydata = model.bodydata;
 
 // Setup to use express.
 const app = express();
