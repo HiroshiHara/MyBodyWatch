6514aef59fc8079bbb137dffed8925c018b4272d
HaraHiroshi <mrbob403@gmail.com>
2020-09-23 23:15:12 +0900

    I founded how to impletent onClick event on C3.js!

--- src/component/Chart.js
+++ src/component/Chart.js
@@ -15,6 +15,7 @@ export class Chart extends Component<Props, State> {
   }
 
   renderChart() {
+    const chartData = this.props.initData;
     c3.generate({
       /**
        * Rendering Chart on this id.
@@ -27,7 +28,7 @@ export class Chart extends Component<Props, State> {
       data: {
         x: "date",
         xFormat: "%Y-%m-%d %H:%M",
-        json: this.props.initData,
+        json: chartData,
         // "y2" define right y-axis for different unit.
         axes: {
           kcal: "y2",
@@ -35,6 +36,16 @@ export class Chart extends Component<Props, State> {
         // Make certain data to different look.
         types: {
           kcal: "bar",
+        },
+        onclick: function (d, i) {
+          console.log("onclick", d, i);
+          const index = d.index;
+          console.log(chartData.weight[index]);
+          console.log(chartData.bmi[index]);
+          console.log(chartData.bfp[index]);
+          console.log(chartData.mm[index]);
+          console.log(chartData.kcal[index]);
+          console.log(chartData.date[index]);
         },
       },
 
